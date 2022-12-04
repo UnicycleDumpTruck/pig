1fa3ea46336b4c12f84386bf419de85303cc3228
Jeff Highsmith <junk@jeffhighsmith.com>
2022-12-04 11:32:05 -0500

    Add receive and ftoa functions
    Copied in functions for receiving with radio and for converting floats to char

--- src/radio.cpp
+++ src/radio.cpp
@@ -90,3 +90,94 @@ void radioSetup()
     digitalWrite(RFM69_CS, HIGH);
     Serial.println("Radio Setup Complete");
 }
+
+void receiveFromCube()
+{
+	selectRadio();  
+	if (rf69.recv((uint8_t*)&eventData, &len) && len == sizeof(eventData))
+	{    
+		char buf[16];
+		String stringBuf = "-00.000";
+		
+		Serial.print("Received: ");
+		Serial.print("c=");
+		Serial.print(itoa(eventData.cubeID, buf, 10));
+		Serial.print(" t=");
+		Serial.print(itoa(eventData.counter, buf, 10));
+		Serial.print(" s=");
+		Serial.print(itoa(eventData.side, buf, 10));
+		Serial.print(" g=");
+		Serial.print(rf69.lastRssi(), DEC);
+		Serial.print(" b=");
+		Serial.print(eventData.batteryVoltage);
+		
+		if (eventData.cubeID == 0) // if message was from gateway
+		{
+			if (eventData.side == 61) { // if message is to drop the pig
+				digitalWrite(RELAY,HIGH); // drop the pig
+				delay(500);
+				digitalWrite(RELAY,LOW);
+			}
+		}
+		Serial.println("");
+	}
+}
+
+// ftoa from http://www.ars-informatica.ca/eclectic/ftoa-convert-a-floating-point-number-to-a-character-array-on-the-arduino/
+void ftoa(float f, char *str, uint8_t precision) {
+  uint8_t i, j, divisor = 1;
+  int8_t log_f;
+  int32_t int_digits = (int)f;             //store the integer digits
+  float decimals;
+  char s1[12];
+
+  memset(str, 0, sizeof(str));  
+  memset(s1, 0, 10);
+
+  if (f < 0) {                             //if a negative number 
+    str[0] = '-';                          //start the char array with '-'
+    f = abs(f);                            //store its positive absolute value
+  }
+  log_f = ceil(log10(f));                  //get number of digits before the decimal
+  if (log_f > 0) {                         //log value > 0 indicates a number > 1
+    if (log_f == precision) {              //if number of digits = significant figures
+      f += 0.5;                            //add 0.5 to round up decimals >= 0.5
+      itoa(f, s1, 10);                     //itoa converts the number to a char array
+      strcat(str, s1);                     //add to the number string
+    }
+    else if ((log_f - precision) > 0) {    //if more integer digits than significant digits
+      i = log_f - precision;               //count digits to discard
+      divisor = 10;
+      for (j = 0; j < i; j++) divisor *= 10;    //divisor isolates our desired integer digits 
+      f /= divisor;                             //divide
+      f += 0.5;                            //round when converting to int
+      int_digits = (int)f;
+      int_digits *= divisor;               //and multiply back to the adjusted value
+      itoa(int_digits, s1, 10);
+      strcat(str, s1);
+    }
+    else {                                 //if more precision specified than integer digits,
+      itoa(int_digits, s1, 10);            //convert
+      strcat(str, s1);                     //and append
+    }
+  }
+
+  else {                                   //decimal fractions between 0 and 1: leading 0
+    s1[0] = '0';
+    strcat(str, s1);
+  }
+
+  if (log_f < precision) {                 //if precision exceeds number of integer digits,
+    decimals = f - (int)f;                 //get decimal value as float
+    strcat(str, ".");                      //append decimal point to char array
+
+    i = precision - log_f;                 //number of decimals to read
+    for (j = 0; j < i; j++) {              //for each,
+      decimals *= 10;                      //multiply decimals by 10
+      if (j == (i-1)) decimals += 0.5;     //and if it's the last, add 0.5 to round it
+      itoa((int)decimals, s1, 10);         //convert as integer to character array
+      strcat(str, s1);                     //append to string
+      decimals -= (int)decimals;           //and remove, moving to the next
+    }
+  }
+}
