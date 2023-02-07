d31d2f7ad6d246bd7380558a856cc145de25ad64
Jeff <junk@jeffhighsmith.com>
2023-02-07 10:03:21 -0500

    Built

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.41"
+        #define VERSION "0.1.42"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2022-12-27 12:40:58.130960"
+        #define BUILD_TIMESTAMP "2023-02-03 09:03:10.251751"
     #endif
     
